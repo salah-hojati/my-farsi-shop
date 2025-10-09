151ad0966f09bc61881b2be7f8b955a86b1e05f8
salah <salahedin1362@gmail.com>
2025-10-10 02:18:09 +0330

    پشتیبان وردپرس: 2025-10-10 02:18:08 - 1 صفحه, 12 جدول

--- removeProdVoulum.sh
+++ removeProdVoulum.sh
@@ -4,8 +4,7 @@ docker-compose -f docker-compose.prod.yml down
 # حذف volume مشکل‌ساز
 docker volume rm my-farsi-shop_db_data
 
-# راه‌اندازی مجدد
-docker-compose -f docker-compose.prod.yml up -d
+
 
 # منتظر بمانیم MariaDB کامل بالا بیاد
 sleep 30
