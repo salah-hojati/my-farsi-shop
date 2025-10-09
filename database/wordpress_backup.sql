ec814cbd7c201cf0e41c2fdcccfa1499131acb42
salah <salahedin1362@gmail.com>
2025-10-09 23:29:18 +0330

    WordPress sync: 2025-10-09 23:29:18 -  pages,  tables

--- database/wordpress_backup.sql
+++ database/wordpress_backup.sql
@@ -1,24 +1,4 @@
---mysqldump: [Warning] Using a password on the command line interface can be insecure.
--- MySQL dump 10.13  Distrib 5.7.44, for Linux (x86_64)
---
--- Host: localhost    Database: wordpress
--- ------------------------------------------------------
--- Server version	5.7.44
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `wp_actionscheduler_actions`
---
 
 DROP TABLE IF EXISTS `wp_actionscheduler_actions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
