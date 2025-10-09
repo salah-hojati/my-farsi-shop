e4e07f17f0f17dc1ea23323e89e287bdc8e914eb
salah <salahedin1362@gmail.com>
2025-10-09 23:59:50 +0330

    WordPress sync: 2025-10-09 23:59:50 - ERROR 1146 (42S02) at line 1: Table 'wordpress.wp_posts' doesn't exist pages, 0 tables

--- initInOtherDev.sh
+++ initInOtherDev.sh
@@ -2,8 +2,9 @@
 set -e  # Exit on any error
 
 docker-compose -f docker-compose.dev.yml down
-sudo docker volume rm my-farsi-shop_db_data
-docker-compose -f docker-compose.dev.yml up -d --force-recreate
+docker-compose -f docker-compose.dev.yml up -d
+#sudo docker volume rm my-farsi-shop_db_data
+#docker-compose -f docker-compose.dev.yml up -d --force-recreate
 
 echo "🎯 Starting deployment - Database synchronization..."
 
